238a7a0291a726a37b735cf221f200e1d8503d90
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-09-16 15:37:30 +0200

    hw2_let_type

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+// Task1
+let name ='Джон';
+let admin;
+admin = name;
+alert(`${admin}`);
+
+// Task2
+let planetEarth;
+let userName;
+
+// Task3
+let ten = '10' + 10;
+console.log(ten);
+console.log(typeof ten);
