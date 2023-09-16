a34449a9ca6b0ad5c4a8dbe740c265d342b74260
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-09-16 15:07:29 +0200

    hw1_prompt_alert

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 'use strict';
+let userName = prompt('Привіт, як тебе звати?');
+if (userName){
+    alert(`Привіт, ${userName}! Раді вітати тебе!`);
+}
+else{
+    alert('Як не гарно не вітатись!');
+}
