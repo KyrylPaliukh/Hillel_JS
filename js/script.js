aa297050cb91ab3b385483c5fd4f3a18ccfd8253
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-09-24 12:45:47 +0200

    hw4_update4

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 const USER_NAME = prompt('Привіт, як тебе звати?');
 if (USER_NAME){
-    alert(`Привіт, ${USER_NAME}! Раді вітати тебе, друже!`);
+    alert(`Привіт, ${USER_NAME}! Раді вітати тебе`);
 }
 else{
     alert('Як не гарно не вітатись!');
