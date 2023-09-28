819dca7c887d21fa6f5e3207c8f94f1d32e96423
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-09-28 17:35:11 +0200

    hw4_update4

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 'use strict';
+// Task 1
+let object1= {
+    name: "John",
+    surname: "Smith"
+};
+object1.name = "Pete";
+delete object1.name;
+console.log(object1);
+
+// Task 2
+const user = {
+    name: "John"
+};
+// Це працюватиме?
+user.name = "Pete";
+console.log(user.name);
+// Так, це працюватиме, тому що ми змінюємо властивість в об'єкті, а не змінюємо константу
+
+// Task 3
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+};
+console.log("Працівники та їх зарплати:", salaries);
+let sum = salaries.John + salaries.Ann + salaries.Pete;
+console.log(`Сума всіх зарплат: ${sum}`);
