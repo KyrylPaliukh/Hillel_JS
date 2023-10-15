6ab7e41ad8f399dee292541e452e4adc32f16076
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-10-15 22:30:24 +0200

    hw_21_update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-'use strict';
+const NUMBER = prompt('Введіть число');
+const DEGREE = prompt('Введіть ступень');
+function resultDegree(number, degree) {
+  if (number === null) {
+    alert('Ви не вказали число');
+  } else if (isNaN(number)) {
+    alert('Ви не вказали число');
+  } else if (degree === null) {
+    alert('Ви не вказали ступінь');
+  } else if (isNaN(degree)) {
+    alert('Ви не вказали ступінь');
+  }
+  return number ** degree;
+}
+const result = resultDegree(NUMBER, DEGREE);
+if (!isNaN(result)) {
+  alert(result);
+} else {
+  alert('Операція неможлива');
+}
