854f00435df4096488261218e1f98f91e8ad3b50
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-10-15 19:53:51 +0200

    hw_20_update1.0

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function generateKey(length, characters) {
-    let result = ''
-    const charactersLength = characters.length
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * charactersLength)
-        result += characters.charAt(randomIndex)
-    }
-    return result
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i += 1) {
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
 }
-const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
-const key = generateKey(16, characters)
-console.log(key)
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(16, characters);
+console.log(key);
