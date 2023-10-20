908cdf8e48a667f191f1be7349c17003aec09993
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-10-20 16:38:08 +0200

    hw_24_update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-'use strict';
+const mainFunction = function (callback) {
+  const number1 = +prompt('Введіть число');
+  const number2 = +prompt('Введіть ступінь числа');
+  callback(number1, number2);
+};
+const exponentiation = function (number, degree) {
+  const result = number ** degree;
+  alert(`Результат: ${result}`);
+};
+
+mainFunction(exponentiation);
