554e7a83af7155d2b30776044f23346425e52984
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-10-28 19:23:03 +0200

    hw_25

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-'use strict';
+const length = +prompt('Введіть довжину масиву');
+const array = [];
+for (let i = 0; i < length; i++) {
+  const element = +prompt(`Введіть елемент масиву ${i}`);
+  array.push(element);
+}
+console.log('Масив:', array);
+array.sort((a, b) => a - b);
+console.log('Відсортований масив:', array);
+array.splice(2, 3);
+console.log('Масив після видалення:', array);
