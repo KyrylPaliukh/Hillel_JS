2c4d2bfc01e8e8a3155d07501e5a955efa8af0b0
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-11-05 19:08:21 +0100

    hw_29

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-'use strict';
+function getFactorial(number) {
+  if (number === 0) {
+    return 1;
+  }
+  return number * getFactorial(number - 1);
+}
+const factorialOf3 = getFactorial(3);
+console.log(factorialOf3);
+
+const num = +prompt('Введіть число, яке потрібно звести у ступінь:');
+const degree = +prompt('Введіть ступінь, в яке потрібно звести число:');
+function pow(num, degree) {
+  if (degree === 0) {
+    return 1;
+  }
+  return num * pow(num, degree - 1);
+}
+const result = pow(num, degree);
+console.log(`Результат: ${result}`);
+
+function add(a, b) {
+  if (b === 0) {
+    return a;
+  }
+  return add(a ^ b, (a & b) << 1);
+}
+
+const sums = add(5, 3);
+console.log(sums);
