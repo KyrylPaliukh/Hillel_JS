89635768f1bd824f4157c3b47d5b3b5f61cceb06
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-11-05 20:03:44 +0100

    hw_31

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-'use strict';
+function counter(number = 0) {
+  let value = number;
+  return {
+    increase() {
+      value++;
+    },
+    decrease() {
+      value--;
+    },
+    value() {
+      return value;
+    },
+  };
+}
+const counter1 = counter(10);
+const counter2 = counter(5);
+
+counter1.increase();
+counter1.increase();
+
+counter2.increase();
+counter2.decrease();
+console.log(counter1.value());
+console.log(counter2.value());
