4bf369d34e0cd1e4a8907b840c77c0f2c984ac4a
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-11-05 18:28:31 +0100

    hw_30

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-'use strict';
+const arr = [
+  1, 2, 3, 4, {},
+  [1, 2, 3],
+  [1, 2, 3],
+  [3, 4,
+    [5, 6, 7],
+  ],
+];
+
+function outerFlat(arr) {
+  const res = [];
+  function flat(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i])) {
+        flat(arr[i]);
+      } else {
+        res.push(arr[i]);
+      }
+    }
+  }
+
+  flat(arr);
+  return res;
+}
+console.log(outerFlat(arr));
+console.log(outerFlat(arr));
