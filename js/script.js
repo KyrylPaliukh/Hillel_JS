c30d80571eae81a5e33fe9e6c62650333362aa70
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-11-11 19:36:00 +0100

    hw_32

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-'use strict';
+function customBind(func, context) {
+  return function (...args) {
+    return func.apply(context, args);
+  };
+}
+
+const obj = {
+  x: 10,
+  getX() {
+    return this.x;
+  },
+};
+
+const customBoundFunc = customBind(obj.getX, obj);
+console.log(customBoundFunc());
