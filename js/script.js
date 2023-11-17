77244ae3b23e8d5d92e927c2c556a829655b64fb
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-11-17 13:38:24 +0100

    hw_34

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-'use strict';
+function insertWordInto(initialString) {
+  const words = initialString.split(' ');
+  return function (wordToInsert) {
+    // Перевірити, чи вже існують слова в рядку
+    if (words.length === 0) {
+      return wordToInsert;
+    }
+    const randomIndex = Math.floor(Math.random() * (words.length - 1)) + 1;
+    words.splice(randomIndex, 0, wordToInsert);
+    const resultString = words.join(' ');
+
+    return resultString;
+  };
+}
+const insert = insertWordInto('hello world');
+const result = insert('Dnipro');
+console.log(result);
+
+const secondResult = insert('Kyiv');
+console.log(secondResult);
+
+const thirdResult = insert('Lviv');
+console.log(thirdResult);
