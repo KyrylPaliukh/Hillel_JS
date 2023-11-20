0d074abc1fc3aa8b47d5056070479f4476849686
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-11-20 20:10:17 +0100

    hw_37

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-'use strict';
+function generateList(array) {
+  const ul = document.createElement('ul');
+  for (const item of array) {
+    const li = document.createElement('li');
+    if (Array.isArray(item)) {
+      li.appendChild(generateList(item));
+    } else {
+      li.textContent = item;
+    }
+    ul.appendChild(li);
+  }
+  return ul;
+}
+const data = [1, 2, [1.1, 1.2, 1.3], 3];
+const list = generateList(data);
+document.body.appendChild(list);
