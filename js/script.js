2818f649fc44b01452c7ff113e665d54dd7ad3e8
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-11-20 20:41:06 +0100

    hw_38

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-'use strict';
+function createTable() {
+  const myTable = document.createElement('table');
+  let count = 1;
+  for (let i = 1; i <= 10; i++) {
+    const tr = document.createElement('tr');
+    for (let j = 1; j <= 10; j++) {
+      const td = document.createElement('td');
+      td.textContent = count++;
+      tr.append(td);
+    }
+    myTable.append(tr);
+  }
+  document.body.append(myTable);
+}
+createTable();
