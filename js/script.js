e66aa17e655b2af7d5fc1b06add72f2ab727c1f6
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-11-23 20:35:18 +0100

    hw_39

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-'use strict';
+const text = document.querySelector('[data-text]');
+const div = document.querySelector('[data-div]');
+text.addEventListener('focus', () => {
+  div.style.display = 'block';
+});
+text.addEventListener('blur', () => {
+  div.style.display = 'none';
+});
