ba029095603cdbd94d8997ef8a2c16c76f4984fa
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-11-26 18:21:45 +0100

    hw_41

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const img = document.querySelector('[data-img]');
-  const imgFolder = '../img/';
-  const numberOfImg = 9;
-  const randomImgIndex = Math.floor(Math.random() * numberOfImg) + 1;
-  const imgPath = `${imgFolder}${randomImgIndex}.jpg`;
-  img.src = imgPath;
+const images = document.querySelectorAll('[data-img] img');
+images.forEach((img) => {
+  img.addEventListener('click', () => {
+    if (img.style.width === '640px' && img.style.height === '426px') {
+      img.style.width = '320px';
+      img.style.height = '213px';
+    } else {
+      img.style.width = '640px';
+      img.style.height = '426px';
+    }
+  });
+});
+const tabs = document.querySelectorAll('.tab');
+const contents = document.querySelectorAll('.content');
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const targetId = tab.getAttribute('data-tab');
+    contents.forEach((content) => {
+      content.style.display = 'none';
+    });
+    document.getElementById(targetId).style.display = 'block';
+  });
 });
