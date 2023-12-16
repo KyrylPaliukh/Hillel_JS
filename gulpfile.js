489330305e777f0cf6d42cdf9b741d1fa5282a8c
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2023-12-16 12:40:00 +0100

    hw_47

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,30 @@ const copyScss = () => src('dist/**/*.scss')
   .pipe(dest('build/styles'));
 
 exports.copy = copyScss;
+
+const { parallel } = require('gulp');
+
+const sassCompile = (done) => {
+  console.log('Compile SASS to CSS');
+
+  done();
+};
+
+const pugCompile = (done) => {
+  console.log('Compile Pug to HTML');
+
+  done();
+};
+
+const imagesOptimize = (done) => {
+  console.log('Optimize Images');
+
+  done();
+};
+
+exports.layoutCompile = parallel(sassCompile, pugCompile);
+exports.assetsOptimize = imagesOptimize;
+
+const { series } = require('gulp');
+
+exports.default = series(copyScss, exports.layoutCompile, exports.assetsOptimize);
