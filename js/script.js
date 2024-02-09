e315bee84e0fc416a51e59c84ceaec2a386c68f9
KyrylPaliukh <paliukh.kyryl@gmail.com>
2024-02-09 18:30:24 +0100

    hw_68

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-'use strict';
-hhhhh
+function trackObjects() {
+  const trackedObjects = new WeakSet();
+
+  return function (obj) {
+    if (trackedObjects.has(obj)) {
+      return true;
+    }
+    trackedObjects.add(obj);
+    return false;
+  };
+}
+const track = trackObjects();
+
+const obj1 = { name: 'Object 1' };
+const obj2 = { name: 'Object 2' };
+
+console.log(track(obj1));
+console.log(track(obj1));
+console.log(track(obj2));
