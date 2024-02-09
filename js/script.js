1af9250516ba7e23ed894ec7ed797f69c8034e17
KyrylPaliukh <paliukh.kyryl@gmail.com>
2024-02-09 18:54:18 +0100

    hw_66

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
-'use strict';
-hhhhh
+function analyzeText(text) {
+  const words = text.toLowerCase().match(/\b\w+\b/g);
+
+  const uniqueWordsSet = new Set(words);
+
+  return {
+    uniqueWordsCount: uniqueWordsSet.size,
+    uniqueWords: Array.from(uniqueWordsSet),
+  };
+}
+const text = 'Hello, JavaScript!!! test - text.! hello';
+const analysisResult = analyzeText(text);
+console.log(analysisResult.uniqueWordsCount);
+console.log(analysisResult.uniqueWords);
